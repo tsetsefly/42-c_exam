1ef2d2f4b73fc408c3652294d557bae176c6c03a
tsetsefly <tsetsefly@gmail.com>
2016-11-21 17:27:35 -0800

    fixing typo in itoa_base

--- rendu_correct/05_ft_itoa_base.c
+++ rendu_correct/05_ft_itoa_base.c
@@ -83,7 +83,7 @@ char	*ft_itoa_base(int value, int base)
 // 	printf("Base 10: -1567       = %s [-1567]\n", ft_itoa_base(-1567, 10));
 // 	printf("Base 2:  1567        = %s [11000011111]\n", ft_itoa_base(1567, 2));
 // 	printf("Base 8: 1567         = %s [3037]\n", ft_itoa_base(1567, 8));
-// 	printf("Base 16: 1567        = %s [61f]\n", ft_itoa_base(1567, 16));
+// 	printf("Base 16: 1567        = %s [61F]\n", ft_itoa_base(1567, 16));
 // 	printf("Base 10: -2147483648 = %s [-2147483648]\n", ft_itoa_base(-2147483648, 10));
 // 	printf("Base 2: 0            = %s [0]\n", ft_itoa_base(0, 2));
 // 	return (0);
